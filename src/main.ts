0b49bf08988a9ca5671333fc76aa857af3a48401
Isola Pelumi <pelumiisola87@gmail.com>
2024-10-28 02:14:03 +0100

    feat: swagger docs added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const sawggerConfig = new DocumentBuilder()
+    .setTitle('Task Manager documentation')
+    .setDescription('Contains all API related to the task manager')
+    .setVersion('v1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, sawggerConfig);
+  SwaggerModule.setup('/v1', app, document);
+  
   app.enableCors({
     origin: [
       'http://localhost:3000',
