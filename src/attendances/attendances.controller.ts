1076fbc12d31ede242952f54cdaf480483374cdc
Isola Pelumi <pelumiisola87@gmail.com>
2024-12-17 22:57:45 +0100

    feat: swagger authentication setup for attnendance

--- src/attendances/attendances.controller.ts
+++ src/attendances/attendances.controller.ts
@@ -10,7 +10,10 @@ import {
 import { AttendancesService } from './attendances.service';
 import { CreateAttendanceDto } from './dto/create-attendance.dto';
 import { UpdateAttendanceDto } from './dto/update-attendance.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Attendance')
+@ApiBearerAuth('access-token')
 @Controller('attendances')
 export class AttendancesController {
   constructor(private readonly attendancesService: AttendancesService) {}
