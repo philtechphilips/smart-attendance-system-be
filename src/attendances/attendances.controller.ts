f60e0cd83e40f0785c3c952bad0ff3f8ab94c0eb
Isola Pelumi <pelumiisola87@gmail.com>
2024-12-28 18:33:28 +0100

    feat: all attendance service

--- src/attendances/attendances.controller.ts
+++ src/attendances/attendances.controller.ts
@@ -6,6 +6,8 @@ import {
   Patch,
   Param,
   Delete,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { AttendancesService } from './attendances.service';
 import { CreateAttendanceDto } from './dto/create-attendance.dto';
@@ -14,11 +16,12 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Attendance')
 @ApiBearerAuth('access-token')
-@Controller('attendances')
+@Controller('/v1/attendances')
 export class AttendancesController {
   constructor(private readonly attendancesService: AttendancesService) {}
 
   @Post()
+  @UsePipes(ValidationPipe)
   create(@Body() createAttendanceDto: CreateAttendanceDto) {
     return this.attendancesService.createAttendance(createAttendanceDto);
   }
--- src/attendances/attendances.module.ts
+++ src/attendances/attendances.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AttendancesService } from './attendances.service';
 import { AttendancesController } from './attendances.controller';
+import { Student } from 'src/students/entities/student.entity';
+import { Attendance } from './entities/attendance.entity';
+import { Course } from 'src/courses/entities/course.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Attendance, Student, Course])],
   controllers: [AttendancesController],
   providers: [AttendancesService],
 })
